3e274e7e17c9202fbd979e5360eb5b0570145e91
Evgeny Ostrovsky <evostrov@microsoft.com>
2021-04-15 12:06:56 +0300

    HW 1, Question 1

--- HW_1/Q_1/main.py
+++ HW_1/Q_1/main.py
@@ -13,6 +13,8 @@ SIN_60 = math.sin(math.pi / 3)
 COS_30 = math.cos(math.pi / 6)
 SIN_30 = math.sin(math.pi / 6)
 
+TRIANGLE_ANGLES = 120
+
 
 def plot_graphs():
 
@@ -51,7 +53,7 @@ def hough_transfom():
     # 2. edge angle is accordingly to graident direction
     # 3. given the triangle edge length, iterate over all possible triangles centers given the image point
 
-    possible_triangles = np.zeros((image_length_y, image_length_x))
+    possible_triangles = np.zeros((image_length_y, image_length_x, TRIANGLE_ANGLES))
 
     for x in range(image_length_x):
         for y in range(image_length_y):
@@ -76,18 +78,24 @@ def hough_transfom():
                 center_x = int(x + i * cos_direction_edge + edge_to_center * cos_direction_normal)
 
                 if not ((center_x < 0) or (center_x >= image_length_x) or (center_y < 0) or (center_y >= image_length_y)):
-                    possible_triangles[center_y, center_x] += 1
+                    possible_triangles[center_y, center_x, direction % TRIANGLE_ANGLES] += 1
 
-    center_y, center_x = np.unravel_index(np.argmax(possible_triangles), possible_triangles.shape)
+    center_y, center_x, triangle_angel = np.unravel_index(np.argmax(possible_triangles), possible_triangles.shape)
 
-    x1 = center_x - edge_length_half
-    y1 = center_y - edge_to_center
-    x2 = center_x + edge_length_half
-    y2 = y1
-    x3 = center_x
-    y3 = y1 + edge_height
-    # triangle = np.array([[x1, y1], [x2, y2], [x3, y3]])
-    triangle = np.array([[center_x - 10, center_y - 10], [center_x - 10, center_y + 10], [center_x + 10, center_y + 10], [center_x + 10, center_y - 10]])
+    angle_1 = triangle_angel
+    angle_2 = triangle_angel + 120
+    angle_3 = triangle_angel + 240
+
+    x1 = center_x + (edge_height - edge_to_center) * math.cos(angle_1 * math.pi / 180)
+    y1 = center_y + (edge_height - edge_to_center) * math.sin(angle_1 * math.pi / 180)
+
+    x2 = center_x + (edge_height - edge_to_center) * math.cos(angle_2 * math.pi / 180)
+    y2 = center_y + (edge_height - edge_to_center) * math.sin(angle_2 * math.pi / 180)
+
+    x3 = center_x + (edge_height - edge_to_center) * math.cos(angle_3 * math.pi / 180)
+    y3 = center_y + (edge_height - edge_to_center) * math.sin(angle_3 * math.pi / 180)
+
+    triangle = np.array([[x1, y1], [x2, y2], [x3, y3]])
 
     plt.subplot(121)
     plt.imshow(image, cmap='gray')
@@ -97,8 +105,8 @@ def hough_transfom():
     plt.gca().add_patch(plt.Polygon(triangle, facecolor="none", edgecolor='blue'))
 
     plt.subplot(122)
-    plt.imshow(possible_triangles, cmap='gray')
-    plt.title('Hough Transform')
+    plt.imshow(possible_triangles[:, :, triangle_angel], cmap='gray')
+    plt.title(f'Hough Transform\nAngle: {triangle_angel}')
     plt.xticks([])
     plt.yticks([])
 
@@ -109,8 +117,8 @@ def hough_transfom():
 if __name__ == '__main__':
 
     # image_name = 'test_1.jpg'
-    # edge_length = 180
-    image_name = 'test_2.jpg'
+    # image_name = 'test_2.jpg'
+    image_name = 'test_3.jpg'
     edge_length = 180
 
     threshold_1 = 100
